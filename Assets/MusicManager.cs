78ffaebd7a8436c96e6c50ddb23db351a974322f
Digx7 <37136301+Digx7@users.noreply.github.com>
2021-08-01 20:19:51 -0700

    Making Some Progress on Sound design

--- Assets/MusicManager.cs
+++ Assets/MusicManager.cs
@@ -7,16 +7,40 @@ public class MusicManager : MonoBehaviour
 {
 
     [SerializeField] private string nameOfSceneToDestroyOn;
+    [SerializeField] private List<LevelAndSongParing> LevelAndSongList;
 
     // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(gameObject);
+        playAll();
+        pauseAll();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
       if(SceneManager.GetActiveScene().name == nameOfSceneToDestroyOn) Destroy(gameObject);
+      //checkIfSongShouldPlay(SceneManager.GetActiveScene().name);
+    }
+
+    void checkIfSongShouldPlay(string currentSceneName){
+      foreach(LevelAndSongParing paring in LevelAndSongList){
+        if(currentSceneName == paring.NameOfSceneToStartOn){
+          pauseAll();
+          paring.MusicAudioSource.UnPause();
+        }
+      }
+    }
+
+    void pauseAll(){
+      foreach(LevelAndSongParing paring in LevelAndSongList){
+        paring.MusicAudioSource.Pause();
+      }
+    }
+
+    void playAll(){
+      foreach(LevelAndSongParing paring in LevelAndSongList){
+        paring.MusicAudioSource.Play();
+      }
     }
 }
